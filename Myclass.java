90c683e56bc9272fd87f9b5a59e2e3deaecb0c7c
jiljith <sruthijiljith@gmail.com>
2019-11-15 23:19:55 +0530

    added third commit

--- Myclass.java
+++ Myclass.java
@@ -4,8 +4,9 @@ public static void main(String args[])
 {
 doStuff();
 }
-public static doStuff()
+public static String doStuff()
 {
 System.out.println("hello this is from method");
+return "hello";
 }
 }
